4eb4fb2b45711855fba460cf1dc55e97b72370b3
houdawei <houdawei@haohantech.net>
2025-01-16 22:19:05 +0800

    adad

--- js/wztydt21.js
+++ js/wztydt21.js
@@ -1,8 +1,9 @@
 document.write("<ins class=\"adsbygoogle\"\n" +
   "             style=\"display:block\"\n" +
-  "             data-ad-format=\"autorelaxed\"\n" +
+  "             data-ad-format=\"auto\"\n" +
+  "             data-full-width-responsive=\"true\"\n" +
   "             data-ad-client=\"ca-pub-9761395598322566\"\n" +
-  "             data-ad-slot=\"6980949150\"></ins>\n" +
+  "             data-ad-slot=\"8964625412\"></ins>\n" +
   "        <script>\n" +
   "          (adsbygoogle = window.adsbygoogle || []).push({});\n" +
   "        </script>")
