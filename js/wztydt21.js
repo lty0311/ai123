075a1fd83a9d854e30c33519602ad356034e0c5b
houdawei <houdawei@haohantech.net>
2025-01-16 22:12:56 +0800

    链接

--- js/wztydt21.js
+++ js/wztydt21.js
@@ -1,1 +1,8 @@
-document.write("<ins class=\"adsbygoogle\"     style=\"display:block\"     data-ad-format=\"fluid\"     data-ad-layout-key=\"-6t+ed+2i-1n-4w\"     data-ad-client=\"ca-pub-0888257628527075\"     data-ad-slot=\"6567557862\"></ins><script>    (adsbygoogle = window.adsbygoogle || []).push({});</script>")
+document.write("<ins class=\"adsbygoogle\"\n" +
+  "             style=\"display:block\"\n" +
+  "             data-ad-format=\"autorelaxed\"\n" +
+  "             data-ad-client=\"ca-pub-9761395598322566\"\n" +
+  "             data-ad-slot=\"6980949150\"></ins>\n" +
+  "        <script>\n" +
+  "          (adsbygoogle = window.adsbygoogle || []).push({});\n" +
+  "        </script>")
